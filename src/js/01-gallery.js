2ecec9f54283ca6249812dbe5a2ac4730ea962f9
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2023-06-14 17:23:37 +0200

    create gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,6 +2,24 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryElement = document.querySelector('ul.gallery');
+
+const galleryItemsElements = galleryItems
+  .map(
+    ({ preview, original, description }) => `
+        <li>
+            <a class="gallery__item" href="${original}">
+               <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+        </li>
+    `
+  )
+  .join('');
+
+galleryElement.insertAdjacentHTML('beforeend', galleryItemsElements);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
