ee55f9d87bb1a9443b129b2f07f60d5a08109775
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2023-06-15 15:10:24 +0200

    add event listener 'timeupdate' to track video playback time

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,9 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+// śledzenie aktualizacji odtwarzania video
+player.on('timeupdate', function (data) {
+  console.log(data);
+});
